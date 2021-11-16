7e5b1ce6f740205b69e11ed9010972aa37a96900
xiaohei0131 <46743384+xiaohei0131@users.noreply.github.com>
2021-11-16 11:27:00 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api':{
+        target:'http://10.1.203.146:8088',
+        changeOrigin: true,
+        pathRewrite:{
+          '^/api':'',
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
